e6a9cecd69832226df639f62f4debe1c0ce10aa5
YaminHossain <yhossain192117@bscse.uiu.ac.bd>
2023-03-02 16:33:55 +0600

    little change again

--- arrayOfObjects.js
+++ arrayOfObjects.js
@@ -4,18 +4,18 @@ const phones = [
   { name: "Xaomi", color: "Black", price: 35000 },
   { name: "Nokia", color: "Black", price: 47000 },
 ];
- phones.push  ({ name: "Realme", color: "Black", price: 17000 });
- var myPhone=cheapestPhone(phones);
-console.log("Price for my phone will be : ",myPhone," taka");
+phones.push({ name: "Realme", color: "Black", price: 17000 });
+var myPhone = cheapestPhone(phones);
+console.log("Price for my phone will be : ", myPhone, " taka");
 // console.log(phones);
-function cheapestPhone(phones){
-    cheapest=phones[0].price;
-    let phone;
-    for(let i=0;i<phones.length;i++){
-        phone = phones[i];
-        if(phone.price<cheapest){
-            cheapest =phone.price;
-        }
+function cheapestPhone(phones) {
+  let cheapest = phones[0].price;
+  let phone;
+  for (let i = 0; i < phones.length; i++) {
+    phone = phones[i];
+    if (phone.price < cheapest) {
+      cheapest = phone.price;
     }
-    return cheapest;
+  }
+  return cheapest;
 }
